42d483985346aa3349ff544c91275e59b8aac7eb
ujasbhuva <ujasbhuva444@gmail.com>
2023-03-01 14:39:55 +0530

    Added server compare type

--- alembic/env.py
+++ alembic/env.py
@@ -71,7 +71,10 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata, compare_type=True
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            compare_server_default=True,
         )
 
         with context.begin_transaction():
